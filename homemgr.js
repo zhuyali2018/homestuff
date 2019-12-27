200364b8fac57329d38c6ae453ddfa82a4c9335f
Yali Zhu <yali900@gmail.com>
2019-12-27 18:55:17 -0500

    Version 6: add record to homestuff.inf

--- homemgr.js
+++ homemgr.js
@@ -1,47 +1,81 @@
 var http = require('http');
-var dt = require('./yalimodule1');
 var url = require('url');
 var fs = require('fs');
-
 var formidable = require('formidable');
-http.createServer(function (req, res) {
+
+//a file checker to make sure no overwriting of an existing file
+function getGoodFn(path){
+  var fn=path;
+  var i=1;
+  for(;;){
+    try {
+      if (fs.existsSync(fn)) {
+        if ( i>100) //try 100 times
+          return "badname_"+i;
+        fn=path+"_"+i;
+      } else{
+        return fn;
+      }
+    }catch(err) {
+      console.error(err);
+    }
+    i++;
+  }
+}
+
+// response with a file uploading form
+function response8(req,res){
+   console.log("In function response");
+   res.writeHead(200, {'Content-Type': 'text/html'});
+   res.write("path: "+req.url);
+
+   fs.readFile('addstuff.html', function(err, data) {
+     res.write(data);
+     res.end();
+   });
+}
+
+const inffile="homestuff.inf";
+function add_record(line){
+   console.log("added: "+line);
+   fs.appendFile(inffile, line, (err) => {
+     if (err) console.log(err);
+     console.log("Successfully added to File.");
+   });
+}
+function requestHandler(req, res) {
   console.log("debug 1:" + req.url)
   if (req.url == '/fileupload') {
-    var q = url.parse(req.url, true).query;
+    //var q = url.parse(req.url, true).query;
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-      //var imgname = unescape(fields.imagename);
+      var parentID= fields.parentID;
+      var itemID=fields.itemID;
+      var itemName=fields.name;
+      var desp=fields.description;
       var imgname = fields.imagename;
+      var orgname = files.filetoupload.name;
+      if(imgname == ""){
+         imgname=orgname;
+      }
       var oldpath = files.filetoupload.path;
-      var newpath = '/Users/yali/homestuff/images/' + imgname;
+      //var newpath = '/Users/yali/homestuff/images/' + imgname;
+      var newpath = './images/' + imgname;
+      var newpath1= getGoodFn(newpath);
       console.log("debug 4: "+imgname);
-      //console.log(oldpath);
-      //console.log(newpath);
-      fs.rename(oldpath, newpath, function (err) {
+      console.log("oldpath: "+oldpath);
+      console.log("newpath: "+newpath1);
+      console.log("orgname: "+orgname);
+      add_record(parentID+ "^" +itemID+ "^" +itemName+"^"+desp+"^"+newpath1+"\n");
+      fs.rename(oldpath, newpath1, function (err) {
         if (err) throw err;
       });
-      fs.readFile('demofile2.html', function(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-      });
+      response8(req,res);
     });
   }else{
     console.log("debug 3")
-    res.writeHead(200, {'Content-Type': 'text/html'});
-
-    res.write("The date and time are currently: " + dt.myDateTime());
-    res.write("<br>req.url<br>");
-    res.write(req.url);
- 
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
-    res.write(txt);
-
-
-    fs.readFile('demofile2.html', function(err, data) {
-     res.write(data);
-     res.end();
-    });
+    response8(req,res);
   }
-}).listen(8080);
+}
+
+var server = http.createServer(requestHandler).listen(8080);
